5705dc1bb79539900472d5ce713bd93be9e87cf6
Demian Zubovich <bobobritva@gmail.com>
2021-05-10 09:38:59 +0300

    5th homework done

--- projects/async/functions.js
+++ projects/async/functions.js
@@ -8,7 +8,13 @@
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function delayPromise(seconds) {}
+function delayPromise(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
+}
 
 /*
  Задание 2:
@@ -23,6 +29,37 @@ function delayPromise(seconds) {}
  Пример:
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
-function loadAndSortTowns() {}
+//    function loadAndSortTowns() {
+//     return new Promise(function (resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+//         xhr.responseType = 'json';
+//         xhr.addEventListener('load', () => {
+
+//             resolve(xhr.response.sort(function (a, b) {
+//                 if (a.name > b.name) {
+//                     return 1;
+//                 }
+//                 if (a.name < b.name) {
+//                     return -1;
+//                 }
+//                 return 0;
+//             }));
+
+//         });
+//         xhr.send();
+//     });
+// }
+function loadAndSortTowns() {
+  return new Promise(function (resolve, reject) {
+    fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+      .then((response) => response.json())
+      .then((cities) => {
+        const sortCities = cities.sort((a, b) => a.name.localeCompare(b.name));
+        resolve(sortCities);
+      })
+      .catch((e) => reject(e));
+  });
+}
 
 export { delayPromise, loadAndSortTowns };
